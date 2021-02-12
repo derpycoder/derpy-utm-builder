16c5563fd6fc36a737d9a3dc967f6b7df0e3cbb7
abhijit-kar <reachme@abhijit-kar.com>
2021-02-12 21:47:14 +0530

    Top Section Separated into Another Component

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,11 @@
     import { fly } from "svelte/transition";
     import { flip } from "svelte/animate";
     import copy from "copy-text-to-clipboard";
-    import { initialUTMParams, blogURL, formatOptions, stripSeparatorRegex } from "./common/constants.js";
+    import { initialUTMParams, blogURL, stripSeparatorRegex } from "./common/constants.js";
     import { formatters } from "./common/utils.js";
     import { showConfetti, showFireWorks } from "./common/confetti.js";
     import { initTheme, toggleTheme } from "./common/theme.js";
+    import TopSection from "./components/TopSection.svelte";
 
     let config = {
         url: "",
@@ -180,59 +181,7 @@
 <main class="w-11/12 mb-6 xs:w-10/12 sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-6/12
   2xl:2-5/12">
     <section class="relative p-8 mb-6 bg-white rounded-lg shadow-2xl">
-        <div class="relative flex flex-col justify-between inline-block text-left">
-            <div class="flex mb-5">
-                <label for="company_website" class="inline-flex items-center px-3 px-4 py-2 text-sm text-gray-500
-          border border-r-0 border-gray-300 rounded-l-md bg-gray-50">
-                    <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                        fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0
-              00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1
-              1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0
-              101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1
-              1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" />
-                    </svg>
-                </label>
-                <div class="flex w-full rounded-md shadow-sm">
-                    <input name="url" class="text-gray-600 flex-1 block w-full px-4 py-2 border
-            border-gray-300 rounded-none outline-none focus:ring-1
-            focus:ring-indigo-500 focus:border-indigo-500 rounded-r-md
-            sm:text-sm" bind:value={config.url} placeholder={blogURL} />
-                </div>
-            </div>
-            <div class="grid grid-cols-3 gap-4">
-                <div>
-                    <label class="font-semibold text-gray-500 ml-1" title="Format of Params" for="format">
-                        Format<span class="text-sm text-red-500">*</span>
-                    </label>
-                    <select id="format" name="format" autocomplete="country" bind:value={config.format} class="text-gray-600 w-full block px-3 py-2 text-right bg-white
-            border border-gray-300 rounded-md shadow-sm outline-none
-            appearance-none focus:ring-1 focus:outline-none
-            focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                        {#each formatOptions as option}
-                        <option>{option}</option>
-                        {/each}
-                    </select>
-                </div>
-                <div>
-                    <label class="font-semibold text-gray-500 ml-1" title="Required: Black Friday, Cyber Monday, ..."
-                        for="campaign">
-                        Campaign<span class="text-sm text-red-500">*</span>
-                    </label>
-                    <input name="campaign" id="campaign" class="text-gray-600 w-full px-4 py-2 border border-gray-300
-            rounded-md outline-none focus:ring-1 focus:ring-indigo-500
-            focus:border-indigo-500 sm:text-sm" bind:value={config.campaign} placeholder="Campaign" />
-                </div>
-                <div>
-                    <label class="font-semibold text-gray-500 ml-1" title="Keywords Associated" for="terms">
-                        Terms
-                    </label>
-                    <input name="terms" id="terms" class="text-gray-600 w-full px-4 py-2 border border-gray-300
-            rounded-md outline-none focus:ring-1 focus:ring-indigo-500
-            focus:border-indigo-500 sm:text-sm" bind:value={config.terms} placeholder="Terms" />
-                </div>
-            </div>
-        </div>
+        <TopSection bind:config={config} />
     </section>
 
     <section class="relative p-8 pb-3 bg-white rounded-lg shadow-2xl">
