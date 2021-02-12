99d5457b034249b217cb9ea8dfd5a768eebf84d0
abhijit-kar <reachme@abhijit-kar.com>
2021-02-12 22:54:15 +0530

    Affiliate Section Separated

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,11 @@
     import { onMount } from 'svelte';
     import { blogURL, stripSeparatorRegex } from "./common/constants";
     import { formatters } from "./common/utils";
-    import { showFireWorks } from "./common/confetti";
     import { initTheme, toggleTheme } from "./common/theme";
     import { config, utmParams, builtURL } from "./stores/store";
     import TopSection from "./components/TopSection.svelte";
     import BottomSection from "./components/BottomSection.svelte";
+    import AffiliateSection from "./components/AffiliateSection.svelte";
 
     initTheme();
 
@@ -60,36 +60,7 @@
         7V37C13.7157 37 7 30.2843 7 22z" />
         </svg>
     </button>
-    <section class="flex justify-center p-5 mb-2 mb-6 text-xs text-gray-900 bg-white
-    shadow-2xl bg-opacity-30 w-referral rounded-xl">
-        <div class="w-2/5 mr-2">
-            <a id="affiliate-link" on:click={showFireWorks} href="https://ghost.org?via=derpycoder" target="_blank"
-                rel="noopener">
-                <figure>
-                    <img src="https://d2gdx5nv84sdx2.cloudfront.net/uploads/pq6gukie/marketing_asset/banner/4835/ghost-squircle.png"
-                        alt="Ghost Pro" border="0" class="mb-2" />
-                    <figcaption class="font-medium hover:underline">
-                        Ghost (Pro)
-                    </figcaption>
-                </figure>
-            </a>
-        </div>
-        <div class="flex flex-col justify-around w-4/5">
-            <div>
-                👈 This is an affiliate link for the best blog tool in the world.
-                <br />
-                (Even I use it for my Blog!)
-            </div>
-            <div>
-                See
-                <a class="text-white hover:underline" href="https://www.derpycoder.com/disclosure/" target="_blank"
-                    rel="noopener">
-                    Disclosure Policy
-                </a>
-                for more.
-            </div>
-        </div>
-    </section>
+    <AffiliateSection />
     <p class="text-white">
         Made with ❤️ by
         <a target="_blank" rel="noopener" class="hover:underline" href="https://www.derpycoder.com">
---
+++ src/components/AffiliateSection.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { showFireWorks } from "../common/confetti";
+</script>
+
+<section class="flex justify-center p-5 mb-2 mb-6 text-xs text-gray-900 bg-white
+    shadow-2xl bg-opacity-30 w-referral rounded-xl">
+    <div class="w-2/5 mr-2">
+        <a id="affiliate-link" on:click={showFireWorks} href="https://ghost.org?via=derpycoder" target="_blank"
+            rel="noopener">
+            <figure>
+                <img src="https://d2gdx5nv84sdx2.cloudfront.net/uploads/pq6gukie/marketing_asset/banner/4835/ghost-squircle.png"
+                    alt="Ghost Pro" border="0" class="mb-2" />
+                <figcaption class="font-medium hover:underline">
+                    Ghost (Pro)
+                </figcaption>
+            </figure>
+        </a>
+    </div>
+    <div class="flex flex-col justify-around w-4/5">
+        <div>
+            👈 This is an affiliate link for the best blog tool in the world.
+            <br />
+            (Even I use it for my Blog!)
+        </div>
+        <div>
+            See
+            <a class="text-white hover:underline" href="https://www.derpycoder.com/disclosure/" target="_blank"
+                rel="noopener">
+                Disclosure Policy
+            </a>
+            for more.
+        </div>
+    </div>
+</section>
