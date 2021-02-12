7f5fee5e5793c2cbf57f50699bebaf1433f1c102
abhijit-kar <reachme@abhijit-kar.com>
2021-02-12 21:22:34 +0530

    Hiding Fork Button on Small Devices

--- src/App.svelte
+++ src/App.svelte
@@ -327,7 +327,7 @@
     }
 </script>
 
-<a target="_blank" rel="noopener" href="https://github.com/abhijit-kar/snowtail" style="position: fixed; top: 0; right: 0; border: 0; z-index: 9999; width:
+<a id="fork-btn" class="hidden" target="_blank" rel="noopener" href="https://github.com/abhijit-kar/snowtail" style="position: fixed; top: 0; right: 0; border: 0; z-index: 9999; width:
   149px; height: 149px;">
     <img loading="lazy" width="149" height="149"
         src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
