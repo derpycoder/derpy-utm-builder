d64f42cc8d16bd85b2bac7db4f105dc387c66ed0
abhijit-kar <reachme@abhijit-kar.com>
2021-02-12 17:31:12 +0530

    Added Focus Outline to UTM URL

--- src/App.svelte
+++ src/App.svelte
@@ -276,8 +276,8 @@
             name="campaign"
             id="campaign"
             class="text-gray-600 w-full px-4 py-2 border border-gray-300
-            rounded-none rounded-md outline-none focus:ring-1
-            focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            rounded-md outline-none focus:ring-1 focus:ring-indigo-500
+            focus:border-indigo-500 sm:text-sm"
             bind:value={config.campaign}
             placeholder="Campaign" />
         </div>
@@ -289,8 +289,8 @@
             name="terms"
             id="terms"
             class="text-gray-600 w-full px-4 py-2 border border-gray-300
-            rounded-none rounded-md outline-none focus:ring-1
-            focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            rounded-md outline-none focus:ring-1 focus:ring-indigo-500
+            focus:border-indigo-500 sm:text-sm"
             bind:value={config.terms}
             placeholder="Terms" />
         </div>
@@ -435,7 +435,8 @@
       </table>
     </div>
     <textarea
-      class="w-full text-center text-gray-400 outline-none"
+      class="w-full text-center text-gray-400 outline-none focus:ring-2
+      focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
       id="story"
       name="story"
       rows="5">
