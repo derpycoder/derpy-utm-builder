6fa6d7d5a42a66be8db244d482ba8d5324a0c1ee
abhijit-kar <reachme@abhijit-kar.com>
2021-02-13 10:14:52 +0530

    Removed Unused Variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { onMount } from 'svelte';
     import { initTheme } from "./common";
     import { config, utmParams, builtURL } from "./stores/store";
     import { AffiliateSection, TopSection, BottomSection, GitHubRibbon, ThemeSwitcher, SectionContainer } from "./components";
@@ -13,8 +12,8 @@
         $utmParams = tmp.utmParams;
     }
 
-    const configSubscription = config.subscribe(persistData);
-    const paramsSubscription = utmParams.subscribe(persistData);
+    config.subscribe(persistData);
+    utmParams.subscribe(persistData);
 
     function persistData() {
         localStorage.derpy_utm_builder = JSON.stringify({
@@ -22,13 +21,6 @@
             utmParams: $utmParams,
         });
     }
-
-    onMount(() => {
-        return () => {
-            configSubscription.unsubscribe();
-            paramsSubscription.unsubscribe();
-        };
-    });
 </script>
 
 <GitHubRibbon />
@@ -37,8 +29,7 @@
     <h1 class="text-4xl font-bold text-white">Derpy UTM Builder</h1>
 </header>
 
-<main class="w-11/12 xs:w-10/12 sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-6/12
-  2xl:2-5/12">
+<main class="w-11/12 xs:w-10/12 sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:2-5/12">
     <SectionContainer>
         <TopSection />
     </SectionContainer>
@@ -46,7 +37,7 @@
     <SectionContainer>
         <BottomSection />
         <textarea
-            class="p-2 resize-none w-full text-center text-gray-400 outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
+            class="w-full p-2 text-center text-gray-400 rounded-md outline-none resize-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
             id="story" name="story" rows="3">{$builtURL}</textarea>
     </SectionContainer>
 </main>
--- src/common/utils.js
+++ src/common/utils.js
@@ -5,7 +5,7 @@ export const formatters = {
         if (!val) {
             return "";
         }
-        const titleCase = val
+        return val
             .toLowerCase()
             .replace(stripSeparatorRegex, " ")
             .split(" ")
@@ -13,60 +13,50 @@ export const formatters = {
                 return word.replace(word[0], word[0] && word[0].toUpperCase());
             })
             .join(" ");
-
-        return titleCase;
     },
     "lower case": val => {
         if (!val) {
             return "";
         }
 
-        const smallCase = val
+        return val
             .toLowerCase()
             .replace(stripSeparatorRegex, " ")
             .split(" ")
             .join(" ");
-
-        return smallCase;
     },
     "kebab-case": val => {
         if (!val) {
             return "";
         }
 
-        const kebabCase = val
+        return val
             .toLowerCase()
             .replace(stripSeparatorRegex, " ")
             .split(" ")
             .join("-");
-
-        return kebabCase;
     },
     "plus+case": val => {
         if (!val) {
             return "";
         }
 
-        const plusCase = val
+        return val
             .toLowerCase()
             .replace(stripSeparatorRegex, " ")
             .split(" ")
             .join("+");
-
-        return plusCase;
     },
     snake_case: val => {
         if (!val) {
             return "";
         }
 
-        const snakeCase = val
+        return val
             .toLowerCase()
             .replace(stripSeparatorRegex, " ")
             .split(" ")
             .join("_");
-
-        return snakeCase;
     },
     "fReE ForM": val => val
 };
